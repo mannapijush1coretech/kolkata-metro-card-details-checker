03c2d237b2a012d1c333e3436dcb4279ff648189
mannapijush1coretech <pijushcoretech@gmail.com>
2021-11-08 16:39:14 +0530

    initial build deisgn

--- src/App.js
+++ src/App.js
@@ -5,14 +5,34 @@ import {Button} from 'react-bootstrap';
 
 
 function App() {
+  const [resdef,setResDef]=useState({
+    status:'-200',
+    msg:"Invalid Card!"
+  });
+  const [cardNo,setCardNo]=useState('');
+  const [data,setData]=useState({
+    data:'',
+    loading:true
+  });
 
+  const getCard= async() =>{
+    //const res=await fetch(process.env.API_URL+"");
+    if(cardNo.length==9){
+      console.log(cardNo);
+      const res=await fetch(process.env.REACT_APP_API_URL+"cardNumber="+cardNo+"&reCardNumber="+cardNo);
+      console.log(res);
+    }else{
+      
+    }
+  }
   
   return (
-    <div>
-      <h1></h1>
-      <input type="text" maxLength="9" minlength="9"/>
-      <Button className="mx-2">Get Card Details</Button>
+    <div className="vw-75">
+      <h2>Kolkata Metro Card Utility</h2>
+      <input value={cardNo} onChange={e=>setCardNo(e.target.value)} className="form-control vw-25" type="text" maxLength="9" minLength="9"/>
+      <Button onClick={getCard}className="m-2">Get Card Details</Button>
       <br />
+      <div>{resdef.status=='-200'?resdef.msg:""}</div>
     </div>
   );
 }
